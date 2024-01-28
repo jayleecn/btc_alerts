380d13873240d3bfc99b17f3419dfa9af52efb0d
ChatBar <jayleecn@gmail.com>
2024-01-28 16:50:39 +0800

    add index.html

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const handler = require('./api/index'); // 引入您的 Serverless Function
+// 引入您的 Serverless Function
+const handler = require('./api/index'); 
 
-app.all('*', (req, res) => {
+// 特别处理 API 请求
+app.all('/api/*', (req, res) => {
   handler(req, res);
 });
 
+// 静态文件服务
+app.use(express.static('.')); 
+
+// 为根 URL 返回 index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 监听端口
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
